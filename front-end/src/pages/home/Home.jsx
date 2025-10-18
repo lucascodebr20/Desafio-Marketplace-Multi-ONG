8b97eedef2e5c5c1e5f8bdb9369dd987afebad3e
lucascodebr20 <lucascode@gmail.com>
2025-10-17 21:49:55 -0300

    Install Tailwind

---
+++ front-end/src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+     <div className="flex h-screen items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 text-white text-4xl font-bold">
+      Tailwind está voando 🚀
+    </div>
+  );
+}
+
+export default Home;
