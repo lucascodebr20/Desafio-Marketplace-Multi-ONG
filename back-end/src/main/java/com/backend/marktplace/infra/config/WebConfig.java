488f8c6abbd1086c5eb8f702adb6c79e36c97dda
lucascodebr20 <lucascode@gmail.com>
2025-10-23 12:08:45 -0300

    Adição do localhost:3000 para cors

--- back-end/src/main/java/com/backend/marktplace/infra/config/WebConfig.java
+++ back-end/src/main/java/com/backend/marktplace/infra/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("https://desafio.lucasbuild.xyz/")
+                .allowedOrigins("http://loalhost:3000/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
